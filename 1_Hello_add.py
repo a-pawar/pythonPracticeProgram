ab69aee5c09980b7908c70af9a01b419a0a4e8f7
Anshul Pawar <99128628+a-pawar@users.noreply.github.com>
2022-04-16 16:34:52 +0530

    Update 1_Hello_add.py

--- 1_Hello_add.py
+++ 1_Hello_add.py
@@ -1,18 +1,21 @@
 #Python program to print hello world
-print("Hello Wolrd")
 '''
 print("Hello World")
-i=10
+'''
+print("Hello World")
+i="Hello World"
 print(i)
-a=input("Write a string Stream")
+a=input("Write a string : ")
 print(a)
-'''
-'''
+
+
       
 #Write a python script to ADD two numbers
 a=10
 b=20
 c=a+b
 print(c)
-#print(10+20)
-'''
+
+print(10+20)
+
+
